669df8cc62afa0613f445e07f81ffc99f1bf2cc1
Daniel_i5_pc <danroch1913@gmail.com>
2023-07-13 00:47:42 -0300

    Lógica ok

--- estrutural.js
+++ estrutural.js
@@ -1,31 +1,36 @@
 'use strict'
 let fatorEscolhido;
 let erros = 0;
-
+let acertos = 0;
 
 //------------------------------------------------------
 //Função que  escolhe o fator da tabuada a ser praticada
 function escolher_tabuada(num){
     let btIniciar = document.createElement("button")
-    btIniciar.innerText = "Iniciar"
-    btIniciar.id = "iniciar"
-    btIniciar.onclick = ()=>{botoesResposta()}
-    btIniciar.className = "bts"
-        
-    let apagar = document.getElementsByClassName("bts")
-    for(let i = 0 ; i < apagar.length; i+1){
-        apagar[i].remove();
-    }
-        
+        btIniciar.innerText = "Iniciar"
+        btIniciar.id = "iniciar"
+        btIniciar.onclick = ()=>{
+            document.getElementById("iniciar").remove()
+            botoesResposta()}
+        btIniciar.className = "bts"
     
-
+    limpaBts()
+        
     document.getElementById("cab").innerHTML = num + "  X  ?"
     document.getElementById("painel").insertAdjacentElement("afterbegin",btIniciar)
     document.getElementById("status").innerHTML = "Status" 
 
     return num;
 }
-//-------------------------------------
+
+
+//Remove os botões-----
+function limpaBts(){
+    let apagar = document.getElementsByClassName("bts")
+    for(let i = 0 ; i < apagar.length; i+1){
+        apagar[i].remove();
+}
+}
 
 
 //-------------------------------------------------------------------
@@ -39,7 +44,6 @@ function seed_botoesResposta(){
     document.getElementById("cab").innerHTML = fatorEscolhido + "  X  " + fator_desafio
     opcoes.unshift(fatorEscolhido * fator_desafio)
 
-
     let contador = 1;
     while(contador < 6){
     let fator_opcao = Math.round((Math.random() * range_limite))
@@ -52,42 +56,74 @@ function seed_botoesResposta(){
 }
 
 function botoesResposta(){
+    limpaBts()
+    let palpite;
     let lista_opcoes = seed_botoesResposta();
+    let resultado = lista_opcoes[0];
     lista_opcoes.sort(()=> .5 - Math.random());
-    console.log(lista_opcoes)
-    document.getElementById("iniciar").remove()
-
+    
+  
     let painel = document.getElementById("painel")
 
     let bt1 = document.createElement("button")
     bt1.innerText= lista_opcoes[0]
     bt1.className = "bts"
     bt1.value = lista_opcoes[0]
+    bt1.addEventListener("click", () => { 
+        palpite =  lista_opcoes[0] 
+        verifica_resposta(resultado,palpite)
+        botoesResposta();
+    })
 
     let bt2 = document.createElement("button")
     bt2.innerText= lista_opcoes[1]
     bt2.className = "bts"
     bt2.value = lista_opcoes[1]
+    bt2.addEventListener("click", () => { 
+        palpite =  lista_opcoes[1] 
+        verifica_resposta(resultado,palpite)
+        botoesResposta();
+    })
 
     let bt3 = document.createElement("button")
     bt3.innerText= lista_opcoes[2]
     bt3.className = "bts"
     bt3.value = lista_opcoes[2]
+    bt3.addEventListener("click", () => { 
+        palpite =  lista_opcoes[2] 
+        verifica_resposta(resultado,palpite)
+        botoesResposta();
+    })
 
     let bt4 = document.createElement("button")
     bt4.innerText= lista_opcoes[3]
     bt4.className = "bts"
     bt4.value = lista_opcoes[3]
+    bt4.addEventListener("click", () => { 
+        palpite =  lista_opcoes[3] 
+        verifica_resposta(resultado,palpite)
+        botoesResposta();
+    })
 
     let bt5 = document.createElement("button")
     bt5.innerText= lista_opcoes[4]
     bt5.className = "bts"
     bt5.value = lista_opcoes[4]
+    bt5.addEventListener("click", () => { 
+        palpite =  lista_opcoes[4] 
+        verifica_resposta(resultado,palpite)
+        botoesResposta();
+    })
 
     let bt6 = document.createElement("button")
     bt6.innerText= lista_opcoes[5]
     bt6.className = "bts"
     bt6.value = lista_opcoes[5]
+    bt6.addEventListener("click", () => { 
+        palpite =  lista_opcoes[5] 
+        verifica_resposta(resultado,palpite)
+        botoesResposta();
+    })
 
     painel.insertAdjacentElement("afterbegin",bt1)
     painel.insertAdjacentElement("afterbegin",bt2)
@@ -96,4 +132,35 @@ function botoesResposta(){
     painel.insertAdjacentElement("afterbegin",bt5)
     painel.insertAdjacentElement("afterbegin",bt6)
 
+    verifica_final()
+}
+
+function verifica_resposta(resultado,palpite){
+    let status = document.getElementById("status")
+    let aux = status.innerHTML
+        if(resultado == palpite){
+                status.innerHTML = aux + " O "
+                acertos += 1
+        }else{
+            status.innerHTML = aux + " X "
+                erros += 1
+        }
+    
+}
+
+function verifica_final(){
+    if((erros + acertos)>5){
+        limpaBts()
+        let painel = document.getElementById("painel")
+        let reiniciar = document.createElement("button")
+        reiniciar.innerText= "reiniciar"
+        reiniciar.className = "bts"
+        reiniciar.onclick = ()=>{ document.location = "index.html"}
+        reiniciar.id = "reiniciar"
+
+        painel.innerHTML = "acertou: " + acertos + " Errou : " + erros
+        painel.insertAdjacentElement("beforeend",reiniciar)
+
+
+    }
 }
